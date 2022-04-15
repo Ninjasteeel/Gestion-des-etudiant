a5826f9a3dfcf318b9cf4fa6ff50fce7ac471f88
Charaf Eddine <Charaf Eddine@DESKTOP-TDDGFI4>
2022-04-15 23:23:54 +0200

    question 1-d

--- Gestion des  Etudiants/src/pack1/Etudiant.java
+++ Gestion des  Etudiants/src/pack1/Etudiant.java
@@ -7,7 +7,9 @@ private double note;
 private String nom;
 public Etudiant(int i,String n){ id=i;nom=n;note=10;}
 public String toString() {return nom+":"+note;}
-	
+public boolean equals(Object o) {if( ! (o instanceof Etudiant) )  return false;
+return ((Etudiant)o).id==this.id ;
+}
 	
 	
 	
