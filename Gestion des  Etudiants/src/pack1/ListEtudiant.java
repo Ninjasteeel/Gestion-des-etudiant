630e57f2110e96d63c5892eedce835e042feabbe
Charaf Eddine <Charaf Eddine@DESKTOP-TDDGFI4>
2022-04-15 23:29:27 +0200

    2-b

--- Gestion des  Etudiants/src/pack1/ListEtudiant.java
+++ Gestion des  Etudiants/src/pack1/ListEtudiant.java
@@ -2,5 +2,10 @@ package pack1;
 import java.util.ArrayList;
 extends ArrayList<Etudiant>
 public class ListEtudiant {
-
+	public boolean add(Etudiant e) {
+		if(this.contains(e)) {
+			set(indexOf(e),e);return false;
+		}
+		else return super.add(e);
+	}
 }
