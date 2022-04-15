c9d0591bd863b9b1e4e26e660bd36b2c9a6da576
Charaf Eddine <Charaf Eddine@DESKTOP-TDDGFI4>
2022-04-15 23:21:41 +0200

    question 1-b

--- Gestion des  Etudiants/src/pack1/Etudiant.java
+++ Gestion des  Etudiants/src/pack1/Etudiant.java
@@ -5,7 +5,7 @@ public class Etudiant  {
 private int id ;
 private double note;
 private String nom;
-public Etudiant(int i,String n){id=i;nom=n;note=10;}
+public Etudiant(int i,String n){ id=i;nom=n;note=10;}
 
 	
 	
